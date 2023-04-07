8d0e3e6bd2ce25ee47a3eb91ad4b05e14d393d23
Shreyas Mudakavi <shreyasmudak@gmail.com>
2023-04-07 18:35:20 +0530

    added some inital code

---
+++ controller/auth.js
@@ -0,0 +1,7 @@
+const User = require("../models/Users");
+const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
+
+exports.login = async (req, res, next) => {};
+
+exports.signUp = async (req, res, next) => {};
